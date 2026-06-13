6252d4f43342e1ec1380cc3b4dc6197467827450
Fabian Stetter <stetter.fabian@gmx.de>
2026-06-13 16:15:55 +0200

    Initial commit

---
+++ src/main/java/de/regitstudios/survivalGamesReborn/SurvivalGamesReborn.java
@@ -0,0 +1,17 @@
+package de.regitstudios.survivalGamesReborn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SurvivalGamesReborn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
